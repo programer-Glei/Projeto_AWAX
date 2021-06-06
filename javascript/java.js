cca9a36036364da500cd6cdbe900cd00db37ade2
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-06-06 19:48:09 -0300

    proximo comit

--- javascript/java.js
+++ javascript/java.js
@@ -1,1 +1,20 @@
 
+
+var slider = document.querySelector('.sliders')
+
+var inicio;
+
+slider.style.marginLeft = '0vw';
+
+//setInterval(atualizartempo, 8000)
+
+function atualizartempo(){
+    inicio = setInterval(atualizarSlider, 200)
+}
+
+function atualizarSlider(){
+    slider.style.marginLeft = (parseInt(slider.style.marginLeft) - 1) + 'vw';
+    if(parseInt(slider.style.marginLeft) == -100){
+        clearInterval(inicio)
+    }
+}
